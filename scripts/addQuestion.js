6ec25daa6572c264c8a3f12e06492eb12cba6dfe
lch-howger <360000521@qq.com>
2020-03-12 14:58:14 +0000

    update add question and js
    update add question and js

---
+++ scripts/addQuestion.js
@@ -0,0 +1,9 @@
+
+function addQuestion() {
+    // let input_question = document.querySelector("#input_question");
+    // alert(input_question.value)
+    //
+    // let parse = JSON.parse("{\"name\":\"xiao\",\"age\":12};");
+    // alert(parse)
+}
+
