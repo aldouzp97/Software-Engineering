038ebf9a2006fcfcfc616827da59d3adf182e3f2
Rhys <26574277+wjonesrhys@users.noreply.github.com>
2020-04-25 20:11:49 +0100

    changed styling of the editor page

--- scripts/editor_options.js
+++ scripts/editor_options.js
@@ -1,89 +1,127 @@
+let pathArray = window.location.pathname.split("/");
+let title = document.getElementsByClassName("p_title");
+title[0].textContent = "Edit Quiz " + getQuizNumber();
+
 //Return to the homepage.
 function goHome() {
   window.location.href = "/";
 }
 
+//Return to the quiz select page for the editor.
+function anotherQuiz() {
+  window.location.href = "/chooseQuizToEdit";
+}
+
+initRadioList();
+
 // Check the button selected, make sure only one selected, open relative page.
 function validateForm() {
-  let radioButtons = document.getElementsByName("option");
-  let count = countChecked(radioButtons);
+  let number_selected = getWaitingList().length;
 
-  if (count === 0) {
-    alert("You need to select an option");
-  }
-
-  if (count > 1) {
-    alert("Too many selected");
-  }
-
-  if (count === 1) {
-    let xhttp = new XMLHttpRequest();
-    let request_url = "http://localhost:3000/quiz" + getQuizNumber();
-    xhttp.open('POST', request_url);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.onload = function () {
-      if (this.response) {
-        let parsed = JSON.parse(this.response);
-        goToPage(parsed.questions.length);
-      }
+  if (number_selected != null) {
+    if (number_selected === 0) {
+      alert("You need to select an option");
     }
-    xhttp.send();
+
+    if (number_selected === 1) {
+      let xhttp = new XMLHttpRequest();
+      let request_url = "http://localhost:3000/quiz" + getQuizNumber();
+      xhttp.open('POST', request_url);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.onload = function () {
+        if (this.response) {
+          let parsed = JSON.parse(this.response);
+          goToPage(parsed.questions.length);
+        }
+      }
+      xhttp.send();
+    }
   }
 }
 
 //Go to the relevant page
 function goToPage(numberOfQuestions){
   console.log(numberOfQuestions);
-  if (returnSelectedValue() === "a") {
+  if (getWaitingList()[0] === 0) {
     if (numberOfQuestions === 0) {
       alert("No questions currently in the pool");
     } else {
       window.location.href = "/preview/" + getQuizNumber();
     }
-  }else if (returnSelectedValue() === "b") {
+  }else if (getWaitingList()[0] === 1) {
     if (confirm("Doing this will overwrite the current question pool, are you ok with this?")) {
       window.location.href = "/prepareQuiz/"+getQuizNumber();
     }
-  }else if (returnSelectedValue() === "c") {
+  }else if (getWaitingList()[0] === 2) {
     if (numberOfQuestions === 5) {
       alert("Already have the maximum number of questions, please remove one before continuing.");
     }
     if (numberOfQuestions < 5) {
       window.location.href = "/addChoice/"+getQuizNumber();
     }
-  }else if (returnSelectedValue() === "d") {
+  }else if (getWaitingList()[0] === 3) {
     if (numberOfQuestions === 0) {
       alert("No questions currently in the pool");
     } else {
       window.location.href = "/removeQuestion/"+getQuizNumber();
     }
-  }else if (returnSelectedValue() === "e") {
+  }else if (getWaitingList()[0] === 4) {
     window.location.href = "/getStatistics/"+getQuizNumber();
   }
 }
 
-// Count the number of radioButtons checked.
-function countChecked(radioButtons) {
-  let count = 0;
-  for(var i = 0; i < radioButtons.length; i++) {
-    if(radioButtons[i].checked == true) {
-      count += 1;
-    }
-  }
-  return count;
+//Initialise the options on the page.
+function initRadioList() {
+  let options = ["View Current Questions", "Prepare Quiz", "Add Question", "Remove Question", "Display Statistics"];
+  options.forEach(function (option, index) {
+    let p = document.createElement("p");
+    p.setAttribute("class", "item_text");
+    p.textContent = option;
+
+    let img_check = document.createElement("img");
+    img_check.setAttribute("class", "item_check");
+    img_check.setAttribute("src", "/image/ic_circle.png");
+
+    let div_item = document.createElement("div");
+    div_item.setAttribute("class", "item")
+    div_item.append(img_check,p);
+
+    div_item.addEventListener("click", function () {
+      if (waitingList.length < 1) {
+        addItemIntoWaitingList(index,div_item,img_check);
+      } else if (waitingList.length===1 && waitingList.includes(index)) {
+        addItemIntoWaitingList(index,div_item,img_check);
+      } else {
+        alert("You cannot select more than one option at a time.")
+      }
+    });
+
+    let list = document.getElementById("list");
+    list.append(div_item);
+  });
 }
 
-// Return the value of the radio button that's selected.
-function returnSelectedValue() {
-  let radioButtons = document.getElementsByName("option");
-  let selected_value = "";
-  for(var i = 0; i < radioButtons.length; i++) {
-    if(radioButtons[i].checked == true) {
-      selected_value = radioButtons[i].value;
-    }
+let waitingList = [];
+
+//Add current selected to the array of waiting objects.
+function addItemIntoWaitingList(index,div_item,img_check) {
+  if (!waitingList.includes(index)) {
+    waitingList.push(index);
+    div_item.setAttribute("class", "item_selected");
+    img_check.setAttribute("src", "/image/ic_check.png");
+  } else {
+    let new_index = waitingList.indexOf(index);
+    waitingList.splice(new_index, 1);
+    div_item.setAttribute("class", "item");
+    img_check.setAttribute("src", "/image/ic_circle.png");
   }
-  return selected_value;
 }
+
+//Get the current waiting list.
+function getWaitingList() {
+  return waitingList;
+}
+
 
 //Return the currently selected quiz.
 function getQuizNumber() {
